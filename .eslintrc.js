b391bba06d4fbf79ff5bf189e75cc17bada7437d
Jamie Connolly <jamie.connolly@tailify.com>
2019-01-09 11:31:03 +0000

    Use the ESLint configuration on itself

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,6 @@
 'use strict';
 
-const { configs: jestConfig } = require('eslint-plugin-jest');
-
 module.exports = {
+  extends: ['./lib/index.js'],
   root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:node/recommended',
-    'plugin:prettier/recommended',
-  ],
-  plugins: ['node'],
-  overrides: [
-    {
-      files: ['**/__{mocks,tests}__/**', '**/*.{mock,test}.js'],
-      ...jestConfig.recommended,
-      env: {
-        jest: true,
-      },
-    },
-  ],
 };
