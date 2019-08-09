da57b67b58adf5d02c23571a0695fa85108360a3
Jamie Connolly <jamie.connolly@tailify.com>
2019-08-09 13:36:00 +0100

    Don't include eslint-plugin-jest as part of the configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,11 @@
 
 module.exports = {
   extends: ['./lib/index.js'],
+  overrides: [
+    {
+      extends: ['plugin:jest/recommended', 'plugin:jest/style'],
+      files: ['**/*.test.js'],
+    }
+  ],
   root: true,
 };
